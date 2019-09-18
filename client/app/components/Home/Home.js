17656e75ae86166eb336b2a16f6ac6b111286570
F. Rick Reich <frickreich@gmail.com>
2019-09-18 15:03:08 +0200

    Update usermanagement

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -3,56 +3,39 @@
 import React, { Component } from 'react';
 import 'whatwg-fetch';
 
-class Home extends Component {
-  constructor(props) {
-    super(props);
+class Home extends Component 
+{
+    constructor(props) 
+    {
+        super(props);
 
-    this.state = {
-      isLoading: true,
-      token: '',
-      signUpError: '',
-      signInError: '',
-      signInEmail: '',
-      signInPassword: '',
-      signUpEmail: '',
-      signUpPassword: '',
-    };
+        this.state =
+        {
+            isLoading: true,
+            token: '',
+            signUpError: '',
+            signInError: '',
+            signInEmail: '',
+            signInPassword: '',
+            signUpEmail: '',
+            signUpPassword: '',
+        };
 
-    this.onTextboxChangeSignInEmail = this.onTextboxChangeSignInEmail.bind(this);
-    this.onTextboxChangeSignInPassword = this.onTextboxChangeSignInPassword.bind(this);
-    this.onTextboxChangeSignUpEmail = this.onTextboxChangeSignUpEmail.bind(this);
-    this.onTextboxChangeSignUpPassword = this.onTextboxChangeSignUpPassword.bind(this);
+        this.onTextboxChangeSignInEmail = this.onTextboxChangeSignInEmail.bind(this);
+        this.onTextboxChangeSignInPassword = this.onTextboxChangeSignInPassword.bind(this);
+        this.onTextboxChangeSignUpEmail = this.onTextboxChangeSignUpEmail.bind(this);
+        this.onTextboxChangeSignUpPassword = this.onTextboxChangeSignUpPassword.bind(this);
     
-    this.onSignIn = this.onSignIn.bind(this);
-    this.onSignUp = this.onSignUp.bind(this);
-    this.logout = this.logout.bind(this);
-  }
-
-  componentDidMount() {
-    const obj = getFromStorage('the_main_app');
-    if (obj && obj.token) {
-      const { token } = obj;
-      // Verify token
-      fetch('/api/account/verify?token=' + token)
-        .then(res => res.json())
-        .then(json => {
-          if (json.success) {
-            this.setState({
-              token,
-              isLoading: false
-            });
-          } else {
-            this.setState({
-              isLoading: false,
-            });
-          }
-        });
-    } else {
-      this.setState({
-        isLoading: false,
-      });
+        this.onSignIn = this.onSignIn.bind(this);
+        this.onSignUp = this.onSignUp.bind(this);
+        this.logout = this.logout.bind(this);
     }
-  }
+
+    componentDidMount() {
+        this.setState({
+          isLoading: false
+        });
+     }
 
   onTextboxChangeSignInEmail(event) {
     this.setState({

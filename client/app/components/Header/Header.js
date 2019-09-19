1d86ac530e14c2b8ea4450c3a47a01463f26f0a2
F. Rick Reich <frickreich@gmail.com>
2019-09-19 13:35:04 +0200

    Fix login

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -2,17 +2,45 @@
 import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom';
+import 'whatwg-fetch';
+
+import {
+    getFromStorage
+} from '../../utils/storage';
 
 class Header extends Component 
 {
     constructor(props)
     {
         super(props);
+
+        this.state =
+        {
+            token: ''
+        };
     }
 
     componentDidMount()
     {
-        
+        const obj = getFromStorage('gandhi');
+
+        if (obj && obj.token) 
+        {
+            const { token } = obj;
+            
+            // Verify token
+            fetch('/api/account/verify?token=' + token)
+            .then(res => res.json())
+            .then(json => 
+            {
+                if (json.success) 
+                {   
+                    this.setState({
+                        token
+                    });
+                }
+            });
+        }
     }
 
     render()
@@ -26,6 +54,13 @@ class Header extends Component
                 </nav>
         
                 <Link to="/asdf">404 Page</Link>
+
+                {
+                    token ?
+                    <p>User logged in</p>
+                    :
+                    <p>User not logged in</p>
+                }
 
                 <hr />
             </header>
